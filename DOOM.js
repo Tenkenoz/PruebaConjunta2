a6fb6c510ab651c9a537fd5b4bc370504b6da93b
Tenkenoz <epobando1@espe.edu.ec>
2025-01-24 10:03:59 -0500

    Aqui se unieron la parte de la logica y la parte de los estilos

--- DOOM.js
+++ DOOM.js
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <script>
+
         class Libro {
             constructor(id, titulo, autor, genero) {
                 this.id = id;
@@ -92,7 +84,3 @@
             const nombreLibro = document.getElementById('input-libro').value;
             biblioteca.reservarLibro(nombreLibro);
         });
-
-    </script>
-</body>
-</html>
